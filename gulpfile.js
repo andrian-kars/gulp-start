9eb3772e06eb0b617e3c338a1b759087996bef4f
andrian-kars <andrian.karsanashvili@gmail.com>
2021-04-08 23:52:16 +0300

    images

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,7 @@ const concat = require('gulp-concat')
 const browserSync = require('browser-sync').create()
 const uglify = require('gulp-uglify-es').default
 const autoprefixer = require('gulp-autoprefixer')
+const imagemin = require('gulp-imagemin')
 
 const browsersync = () => {
     browserSync.init({
@@ -12,6 +13,22 @@ const browsersync = () => {
             baseDir: 'app/'
         }
     })
+}
+
+const images = () => {
+    return src('app/images/**/*')
+        .pipe(imagemin([
+            imagemin.gifsicle({ interlaced: true }),
+            imagemin.mozjpeg({ quality: 75, progressive: true }),
+            imagemin.optipng({ optimizationLevel: 5 }),
+            imagemin.svgo({
+                plugins: [
+                    { removeViewBox: true },
+                    { cleanupIDs: false }
+                ]
+            })
+        ]))
+        .pipe(dest('dist/images'))
 }
 
 const scripts = () => {
@@ -58,5 +75,6 @@ exports.watching = watching
 exports.browsersync = browsersync
 exports.scripts = scripts
 exports.build = build
+exports.images = images
 
 exports.default = parallel(browsersync, watching)
